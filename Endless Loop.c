011fbcf121c2a1669a64d8a36450afc95ac9b49f
Zerrin ul haq. Siya <134535520+zerrin988@users.noreply.github.com>
2025-02-19 01:26:32 +0100

    Update Endless Loop.c

--- Endless Loop.c
+++ Endless Loop.c
@@ -4,12 +4,12 @@
 int main() 
 {
     int i = 1;
-    for (;;)
+    for (;;) //high-speed for loop
     {
         printf("%d \n", i);
-        fflush(stdout);
+        fflush(stdout); //Flushes output immedeatly
         i++;
         if(i > 30){i = 1;}
-        usleep(1000000); 
+        //usleep(1000000); //This slows down the loop, waits 1 second for each iteration, turn it on if you want to slow down the loop
     }
 }
